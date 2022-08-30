16767fe1a2dccd1cfdb4ce690f000c0916a4b4af
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-30 09:40:08 -0700

    Add files via upload

---
+++ main.kts
@@ -0,0 +1,14 @@
+
+
+class Population:
+    fun populationDensity(Pt,P0,N,I,M,E){
+        Pt = P0 + (N + I) - (M + E)
+    }
+    fun exponentialGrowth(Nt,N0,r,t){
+        Nt = N0 * exp(r * t)
+    }
+    fun logisticGrowth(dN,dt,r,N,K){
+        dN/dt = r * N * ((K-N) / K)
+    }
+
+
